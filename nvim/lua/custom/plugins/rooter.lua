a5101330b8c46875f72f238f1753b619ebe7f4b7
John Ngugi <johnngugi777@gmail.com>
2024-08-22 11:04:09 +0300

    Set correct root directory

--- nvim/lua/custom/plugins/rooter.lua
+++ nvim/lua/custom/plugins/rooter.lua
@@ -6,6 +6,7 @@ return {
     -- Instead of this running every time we open a file, we'll just run it once when Vim starts.
     vim.g.rooter_manual_only = 1
     vim.g.rooter_patterns = { '.git' }
+    vim.g.rooter_change_directory_for_non_project_files = "current"
   end,
   config = function()
     vim.cmd 'Rooter'
