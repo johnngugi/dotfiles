ff44f266b9f98da71c645de5c1431d6c01e69df9
John Ngugi <johnngugi777@gmail.com>
2024-09-02 20:54:09 +0300

    Add file explorer option to alpha

--- nvim/lua/custom/plugins/alpha.lua
+++ nvim/lua/custom/plugins/alpha.lua
@@ -20,6 +20,7 @@ return {
 		-- Set menu
 		dashboard.section.buttons.val = {
 			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+			dashboard.button("\\", "  > Toggle file explorer", "<cmd>Neotree reveal<CR>"),
 			dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
 			dashboard.button("SPC sf", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
 			dashboard.button("SPC sg", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
